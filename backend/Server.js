f9d202a9e323b4bb1277f00a0036ae6ef6588f9b
Tashmith Mesith Hansira Bopage <tashmith.20231087@iit.ac.lk>
2026-09-12 13:18:13 +0530

    fix IPv4 for SMTP connection

--- backend/Server.js
+++ backend/Server.js
@@ -22,6 +22,7 @@ const mailTransporter = process.env.SMTP_HOST && process.env.SMTP_USER && proces
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT || 587),
       secure: process.env.SMTP_SECURE === 'true',
+      family: 4,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
